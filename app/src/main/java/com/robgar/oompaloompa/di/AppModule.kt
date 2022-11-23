c30cac8a4ac6ccb6e0ed0b95e10ed1d15e800254
Rigogp2610 <rgarcia2610p@gmail.com>
2022-11-23 19:09:15 +0100

    Add Internet validation

--- app/src/main/java/com/robgar/oompaloompa/di/AppModule.kt
+++ app/src/main/java/com/robgar/oompaloompa/di/AppModule.kt
@@ -4,8 +4,9 @@ import com.robgar.oompaloompa.data.api.ApiHelper
 import com.robgar.oompaloompa.data.api.RetrofitBuilder
 import com.robgar.oompaloompa.data.repository.OompaLoompaWorkerRepository
 import com.robgar.oompaloompa.data.repository.OompaLoompaWorkersRepository
-import com.robgar.oompaloompa.ui.oompaloompaworkers.OompaLoompaWorkersViewModel
 import com.robgar.oompaloompa.ui.oompaloompaworker.OompaLoompaWorkerViewModel
+import com.robgar.oompaloompa.ui.oompaloompaworkers.OompaLoompaWorkersViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -15,6 +16,6 @@ val appModule = module {
     single { ApiHelper(get()) }
     single { OompaLoompaWorkersRepository(get()) }
     single { OompaLoompaWorkerRepository(get()) }
-    viewModel { OompaLoompaWorkersViewModel(get()) }
-    viewModel { OompaLoompaWorkerViewModel(get()) }
+    viewModel { OompaLoompaWorkersViewModel(get(), androidContext()) }
+    viewModel { OompaLoompaWorkerViewModel(get(), androidContext()) }
 }
--- app/src/main/java/com/robgar/oompaloompa/ui/oompaloompaworker/OompaLoompaWorkerViewModel.kt
+++ app/src/main/java/com/robgar/oompaloompa/ui/oompaloompaworker/OompaLoompaWorkerViewModel.kt
@@ -1,34 +1,43 @@
 package com.robgar.oompaloompa.ui.oompaloompaworker
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.robgar.oompaloompa.R
 import com.robgar.oompaloompa.data.model.OompaLoompaWorker
 import com.robgar.oompaloompa.data.repository.OompaLoompaWorkerRepository
 import com.robgar.oompaloompa.utils.Result
+import com.robgar.oompaloompa.utils.hasInternetConnection
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
-class OompaLoompaWorkerViewModel(private val oompaLoompaWorkerRepository: OompaLoompaWorkerRepository) : ViewModel() {
+class OompaLoompaWorkerViewModel(
+    private val oompaLoompaWorkerRepository: OompaLoompaWorkerRepository,
+    private val context: Context
+) : ViewModel() {
 
     private val _oompaLoompaWorker = MutableLiveData<Result<OompaLoompaWorker>>()
     val oompaLoompaWorker : LiveData<Result<OompaLoompaWorker>>
         get() = _oompaLoompaWorker
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 
-    private val errorGetOompaLoompaWorker = "An error has occured. Try it again later"
-
     fun getOompaLoompaWorker(id: Int) {
-        _oompaLoompaWorker.postValue(Result.Loading)
-        coroutineScope.launch {
-            val oompaLoompaWorker = oompaLoompaWorkerRepository.getWorker(id)
-            try {
-                _oompaLoompaWorker.postValue(Result.Success(data = oompaLoompaWorker))
-            } catch (exception: Exception) {
-                _oompaLoompaWorker.postValue(Result.Error(message = exception.message ?: errorGetOompaLoompaWorker))
+        if (hasInternetConnection(context)) {
+            _oompaLoompaWorker.postValue(Result.Loading)
+            coroutineScope.launch {
+                val oompaLoompaWorker = oompaLoompaWorkerRepository.getWorker(id)
+                try {
+                    _oompaLoompaWorker.postValue(Result.Success(data = oompaLoompaWorker))
+                } catch (exception: Exception) {
+                    _oompaLoompaWorker.postValue(Result.Error(
+                        exception.message ?: context.getString(R.string.error_get_content)))
+                }
             }
+        } else {
+            _oompaLoompaWorker.postValue(Result.Error(context.getString(R.string.error_internet)))
         }
     }
 
